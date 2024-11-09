663a42e790202d33f0288bf8e5b7af2eda6fd65d
sonachine <162111822+sonachine@users.noreply.github.com>
2024-11-09 20:20:26 +0300

    Create crypto_secrets_template.py

---
+++ library/crypto_secrets_template.py
@@ -0,0 +1,22 @@
+e = 65537
+N = 51328431690246050000196200646927542588629192646276628974445855970986472407007
+ct = 9015202564552492364962954854291908723653545972440223723318311631007329746475
+
+
+import math
+import sympy
+# from factordb.factordb import FactorDB
+# from timeout_decorator import timeout
+
+# @timeout(10)
+def main():
+    print(f"log2(N) = {math.log2(N)}")
+    print(f"is N prime? {sympy.isprime(N)}")
+
+    # f = FactorDB(N)
+    # f.connect()
+    # print(f"factors of N: {f.get_factor_list()}") # [p, q]
+
+
+if __name__ == '__main__':
+    main()
